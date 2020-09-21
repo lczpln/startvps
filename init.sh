49d6796b70815d71ddfb54e3b9b360d80aba0b2f
lczpln <37776923+lczpln@users.noreply.github.com>
2020-09-21 15:46:36 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ apt -y install firefox
 apt install tightvncserver
 tightvncserver :1
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip
-./ngrok authtoken 1hhEJLMe16qHbkrKTH6DUoYQpkP_Hz6aMhrtD962CZWvasFj
+./ngrok authtoken YOUR_NGROK_TOKEN
 1hpfhoVfxOOnudx0A7gbee2jajq_4vsvUH3K98y11PUgRHXLQ
 screen ./ngrok tcp 5901
