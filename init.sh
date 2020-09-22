f72a78c501a45caf664781b72b7f5c64366c3498
lczpln <37776923+lczpln@users.noreply.github.com>
2020-09-22 07:50:07 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,10 @@ apt update
 apt -y install screen
 apt -y install xfce4
 apt -y install firefox
+cd ~/Downloads
+wget http://download999.mediafire.com/jldct3zt0f9g/4vr6hu3h0ubcaq2/HitLeap+Viewer.tar.xz
+wget https://socialspinner.net/exchanger/Exchanger-1.0.1-linux.zip
+cd /workspace/VPS/startvps
 apt install tightvncserver
 tightvncserver :1
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip
