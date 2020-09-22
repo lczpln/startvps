0d332c9db6ae40453e138cbbcf24e8e0dfdd8463
lczpln <37776923+lczpln@users.noreply.github.com>
2020-09-22 09:47:51 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,13 +3,10 @@ apt update
 apt -y install screen
 apt -y install xfce4
 apt -y install firefox
-cd ~/Downloads
-wget http://download999.mediafire.com/jldct3zt0f9g/4vr6hu3h0ubcaq2/HitLeap+Viewer.tar.xz
-wget https://socialspinner.net/exchanger/Exchanger-1.0.1-linux.zip
-cd /workspace/VPS/startvps
+wget https://hitleap.com/viewer/download?platform=Linux && tar -xf download?platform=Linux
+./HitLeap-Viewer.desktop &
 apt install tightvncserver
 tightvncserver :1
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip
 ./ngrok authtoken YOUR_NGROK_TOKEN
-1hpfhoVfxOOnudx0A7gbee2jajq_4vsvUH3K98y11PUgRHXLQ
 screen ./ngrok tcp 5901
