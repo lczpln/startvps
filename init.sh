9831c6333d0f5dbfb098073d5b05216bf6e23b42
lczpln <37776923+lczpln@users.noreply.github.com>
2022-01-11 19:12:20 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
-apt apt-get install xfce4 -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install xfce4 -y
 #sudo apt-get install ubuntu-desktop
 sudo adduser lczpln
 sudo usermod -aG sudo,adm lczpln
-sudo apt-get update -y
+sudo apt update -y
 wget https://download.nomachine.com/ download/7.7/Linux/nomachine_7.7.4_1_amd64.deb
 sudo dpkg -i nomachine_7.7.4_1_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
